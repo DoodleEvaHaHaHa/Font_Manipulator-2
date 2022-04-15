ce66a062219ed13627c6150b31a8aa0a7969052e
DoodleEvaHaHaHa <92247115+DoodleEvaHaHaHa@users.noreply.github.com>
2022-04-15 17:31:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function setup()
+{
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 450);
+    canvas.position(570, 100);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded()
+{
+    console.log('PoseNet is Initialized');
+}
+function gotPoses()
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
+function draw()
+{
+    background('#00FF00');
+}
